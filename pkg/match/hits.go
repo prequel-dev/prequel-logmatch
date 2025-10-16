03dabc4d8b6fb11523638ada28768aab4719f3c2
Sean Cunningham <sean@prequel.dev>
2025-10-16 14:13:52 -0400

    Add Idx to hits props.

--- pkg/match/hits.go
+++ pkg/match/hits.go
@@ -1,9 +1,14 @@
 package match
 
+type PropKey struct {
+	Idx int
+	Key string
+}
+
 type Hits struct {
 	Cnt   int
 	Logs  []LogEntry
-	Props map[string]any
+	Props map[PropKey]any
 }
 
 func (h *Hits) PopFront() []LogEntry {
@@ -35,4 +40,26 @@ func (h Hits) Index(i int) []LogEntry {
 		off   = i * sz
 	)
 	return h.Logs[off : off+sz]
+}
+
+// IndexProps returns a map of properties for the given index i, aggregating all entries in h.Props
+// where PropKey.Idx == i. If h.Props is nil or no properties match, it returns nil (not an empty map).
+// Out-of-range indices are not explicitly checked; if no properties match, nil is returned.
+
+func (h Hits) IndexProps(i int) map[string]any {
+	if h.Props == nil || i < 0 || i >= h.Cnt {
+		return nil
+	}
+
+	var m map[string]any
+
+	for k, v := range h.Props {
+		if k.Idx == i {
+			if m == nil {
+				m = make(map[string]any)
+			}
+			m[k.Key] = v
+		}
+	}
+	return m
 }
